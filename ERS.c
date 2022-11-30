e41a5b147c3e3f0ffc93315b408c9872eb00147f
AtikAkbar <atik.akbar02@gmail.com>
2022-11-30 15:46:49 +0600

    seclect your choice

--- ERS.c
+++ ERS.c
@@ -34,15 +34,16 @@ Data of the Employees:
 // long int size = sizeof(e);
 
 // // in the start coordinate will be (0,0)
-COORD cord = {0,0};
+// COORD cord = {0,0};
 
-// // function to set coordinates 
+// // function to set coordinates
 void gotoxy(int x, int y)
 {
+    COORD cord;
     cord.X = x;
     cord.Y = y;
-    SetConsoleCursorPosition(
-        GetStdHandle(STD_OUTPUT_HANDLE),cord);
+    // takes the curretnt given position
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cord);
 }
 
 FILE *fp, *ft;
@@ -55,20 +56,22 @@ int main()
     int choice;
 
     // opening the file
-    // fp = fopen("data.txt","rb+");
+    fp = fopen("data.txt","rb+");
 
     // showing error if file is unable to open
-    // if (fp ==  NULL)
-    // {
-    //     fp = fopen("data.txt","wb+");
-    //     if (fp = NULL)
-    //     {
-    //         printf("\n Cannot open file...");
-    //         //  EXIT_FAILURE
-    //         exit(1);
-    //     } 
-    // }
-     
+    if (fp ==  NULL)
+    {
+        fp = fopen("data.txt","wb+");
+        if (fp = NULL)
+        {
+            printf("\n Cannot open file...");
+            //  EXIT_FAILURE
+            exit(1);
+        }
+    }
+
+    // clearing console
+    system("cls");
     //  colors in console
     system("Color 3F");
     printf("\n\n\n\n\t\t\t\t============="
@@ -90,35 +93,63 @@ int main()
            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
            "~~~");
     printf("\n\t\t\t\t====================="
-           "==============================\n");
-    printf("\n\n\n\t\t\t\t\t\t\t\t\t\t"
+           "===============================\n");
+    printf("\n\n\n\t\t\t\t\t\t\t\t\t\t\t\t"
            "Developer : @Atik_Akbar_Rahat"
            "\n\n\t\t\t\t");
 
     // the exe file will not automaticaly close
     system("pause");
-    // printf("Press any button to continue...");
-    // getchar();
-
+    
     while (1)
     {
         //clearing console
         system("cls");
-        gotoxy(10, 10);
-        printf("\n1. ADD RECORD\n");
+
+        gotoxy(30, 10);
+        printf("1. ADD RECORD\n");
         gotoxy(30, 12);
-        printf("\n2. DELETE RECORD\n");
+        printf("2. DELETE RECORD\n");
         gotoxy(30, 14);
-        printf("\n3. DISPLAY RECORDS\n");
+        printf("3. DISPLAY RECORDS\n");
         gotoxy(30, 16);
-        printf("\n4. MODIFY RECORD\n");
+        printf("4. MODIFY RECORD\n");
         gotoxy(30, 18);
-        printf("\n5. EXIT\n");
+        printf("5. EXIT\n");
         gotoxy(30, 20);
-        printf("\nENTER YOUR CHOICE...\n");
+        printf("ENTER YOUR CHOICE...\n");
         // fflush(stdin);
         scanf("%d", &choice);
     }
     
+    // Switch Case
+    switch (choice)
+    {
+    case 1:
+        // add the records
+        addrecord();
+        break;
+    case 2:
+        // delete the records
+        deleterecord();
+        break;
+    case 3:
+        // Display the records
+        displayrecord();
+        break;
+    case 4:
+        // Modify the records
+        modifyrcord();
+        break;
 
+    case 5:
+        // Closing the file
+        fclose(fp);
+        // EXIT_SUCCESS
+        exit(0);
+        break;
+        
+    default:
+        printf("INVALID CHOICE...\n");
+    }
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,6 @@
-gotoxy(10,
+int x, y;
+
+        // printf("Enter the position of cursor x, y: ");
+        // scanf("%d%d", &x, &y);
+
+        // gotoxy(x, y);
